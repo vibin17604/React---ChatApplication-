75a53671da91603c88d0af444553b2a4c01cdf3e
Vibin K <108916451+vibin17604@users.noreply.github.com>
2023-04-05 15:57:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '06c4affb-95bb-4869-9cce-91bacbcfad07';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm/>;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
+
+
